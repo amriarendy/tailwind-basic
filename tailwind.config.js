e6ab064f62f0831c50d46549424ecbaad0dd485c
amriarendy <amriarendy90@gmail.com>
2023-07-07 14:01:43 +0700

    portofolio tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
-  darkMode: 'class',
+  content: ['index.html'],
   theme: {
-      extend: {
-          spacing: {
-              13: '3.25rem'
-          },
-          fontFamily: {
-              inter: ['Inter']
-          },
-          colors: {
-              luffy: '#bada55',
-              onepiece: '#c0ffee'
-          },
-          animation: {
-              'spin-slow': 'spin 3s linear infinite',
-              'goyang': 'wiggle 1s ease-in-out'
-          },
-          keyframes: {
-              wiggle: {
-                  '0%, 100%': {transform: 'rotate(-3deg)'},
-                  '50%': {transform: 'rotate(3deg)'},
-              },
-          },
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        info: "#14b8a6",
+        dark: "#0f172a"
       },
+      screens: {
+        '2xl': '1320px'
+      },
+    },
   },
   plugins: [],
 }
